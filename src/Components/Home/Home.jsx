a4380d8960e41dce830c1bbd7e87421f6274ac5f
apponislame <11appon11@gmail.com>
2024-07-04 11:22:39 +0600

    contact form added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner";
+import Contact from "./Contact";
 import Experienced from "./Experienced";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Experienced></Experienced>
+            <Contact></Contact>
         </div>
     );
 };
